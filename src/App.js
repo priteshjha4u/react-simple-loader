acdfb265de40515651d96504af9f68b212be0281
Pritesh Jha <priteshjha4u@gmail.com>
2019-02-22 19:59:58 +0530

    work in progress

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <React.Fragment>
+        <div className="row bg-dark text-white shadow p-2">
+          <div className="col-md-12">
+            <h4 className="text-center">React Simple Loader</h4>
+          </div>
+        </div>
+        <div className="row justify-content-center align-items-center">
+          <div className="card">
+            <div className="card-body">
+              <button cls="btn btn-outline-primary ml-2 mb-2">Simple Loader</button>
+              <button cls="btn btn-outline-primary ml-2 mb-2">Loader with text</button>
+              <button cls="btn btn-outline-primary ml-2 mb-2">Loader with text position</button>
+              <button cls="btn btn-outline-primary ml-2 mb-2">Custom color loader</button>
+            </div>
+          </div>
+        </div>
+      </React.Fragment>
     );
   }
 }
